63014f90eab99f45c963ce7bff8c4480e563ed20
409262671 <a910413frank@gmail.com>
2023-10-19 14:28:18 +0800

    firsdt

---
+++ lineBot-project/src/test/java/com/example/lineBot/LineBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lineBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
